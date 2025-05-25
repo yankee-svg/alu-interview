3a204f3c9460c5b356b5a77012c68c0eeb99dc00
yankee-svg <e.barame@alustudent.com>
2025-05-25 14:30:43 +0200

    .

---
+++ rain/0-rain.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Calculate the amount of rain that will be captured after it rains
+"""
+
+
+def rain(walls):
+    
+    if not walls or len(walls) < 3:
+        return 0
+
+    rain = 0
+    for i in range(1, len(walls) - 1):
+        left = max(walls[:i])
+        right = max(walls[i + 1:])
+        min_wall = min(left, right)
+        if walls[i] < min_wall:
+            rain += min_wall - walls[i]
+    return rain
